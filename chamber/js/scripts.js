4343f0ed093c454d5c12411ff24460546a47a9f7
ksantos16 <san21087@byui.edu>
2022-01-28 17:40:49 -0500

    creating Homepage for Chamber of commerce

---
+++ chamber/js/scripts.js
@@ -0,0 +1,54 @@
+
+function toggleMenu() {
+    document.querySelector("#primaryNav").classList.toggle("open");
+    document.querySelector("#hamburgerBtn").classList.toggle("open");
+}
+
+const x = document.querySelector("#hamburgerBtn");
+x.oneclick = toggleMenu;
+
+
+
+// const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
+// document.getElementById('currentdate').textContent=new Date().toLocaleDateString('en-US', options);
+
+const lastmod = document.querySelector('#lastmod');
+lastmod.innerHTML = `Last Modification: ${document.lastModified}`;
+
+//long day names array
+const daynames = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+];
+
+//long month name array
+const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+];
+
+const toadysdate = new Date();
+const dayName = daynames[toadysdate.getDay()];
+const monthName = months[toadysdate.getMonth()];
+const currentdate = `${dayName}, ${monthName} ${toadysdate.getDate()}, ${toadysdate.getFullYear()}`;
+const currentyear = document.querySelector("#currentyear")
+currentyear.innerHTML = `&copy ${toadysdate.getFullYear()} Aberdeen Chamber of Commerce | Kira Santos | WDD 230 Project |`;
+document.getElementById('currentdate').textContent = currentdate;
+// document.querySelector("#currentyear").textContent = year;
+
+
