e81fb23b661eb6ca264a867bcf95a6ee5aafad2a
ksantos16 <san21087@byui.edu>
2022-02-05 15:48:07 -0500

    created banner var

--- lesson5/js/banner.js
+++ lesson5/js/banner.js
@@ -1,8 +1,10 @@
 const bannerDate = new Date()
 const bannerDay = bannerDate.getDay();
+const banner = document.querySelector('.banner');
+
 
 if (bannerDay != 1 || bannerDay != 2) {
-    const banner = document.querySelector('.banner').textContent = banner;
+    document.querySelector('.banner').textContent = banner;
 };
 
 
