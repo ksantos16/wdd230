3eab1e8a5468343fd1c89ec53b854f85abf94e73
ksantos16 <san21087@byui.edu>
2022-02-05 14:51:56 -0500

    made changes

---
+++ lesson5/js/banner.js
@@ -0,0 +1,8 @@
+const bannerDate = new Date('August 19, 1975 23:15:30')
+const bannerDay = bannerDate.getDay();
+
+if (bannerDay != 1 && bannerDay != 2) {
+    const banner = document.querySelector('.banner').textContent = banner;
+};
+
+
