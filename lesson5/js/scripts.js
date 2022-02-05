debb887b12e0ef88bc9f5a9c4db87b516274d214
ksantos16 <san21087@byui.edu>
2022-02-04 22:10:23 -0500

    fixed footer!

---
+++ lesson5/js/scripts.js
@@ -0,0 +1,64 @@
+const primaryNav = document.querySelector("#primaryNav")
+const hamburgerBtn = document.querySelector("#hamburgerBtn")
+
+hamburgerBtn.addEventListener("click", () => {
+    document.querySelector("#primaryNav").classList.toggle("open");
+    document.querySelector("#hamburgerBtn").classList.toggle("open");
+});
+
+
+// const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
+// document.getElementById('currentdate').textContent=new Date().toLocaleDateString('en-US', options);
+
+const lastmod = document.querySelector('#lastmod');
+lastmod.innerHTML = `Last Modification: ${document.lastModified}`;
+
+const lastmod2 = document.querySelector('#lastmod2');
+lastmod2.innerHTML = `Last Modification: ${document.lastModified}`;
+
+const lastmod3 = document.querySelector('#lastmod3');
+lastmod3.innerHTML = `Last Modification: ${document.lastModified}`;
+
+
+//long day names array
+const daynames = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+];
+
+//long month name array
+const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+];
+
+const toadysdate = new Date();
+const dayName = daynames[toadysdate.getDay()];
+const monthName = months[toadysdate.getMonth()];
+const currentdate = `${dayName}, ${monthName} ${toadysdate.getDate()}, ${toadysdate.getFullYear()}`;
+const currentyear = document.querySelector(".currentyear")
+currentyear.innerHTML = toadysdate.getFullYear();
+
+const currentyear2 = document.querySelector("#currentyear2")
+currentyear2.innerHTML = toadysdate.getFullYear();
+
+const currentyear3 = document.querySelector("#currentyear3")
+currentyear3.innerHTML = toadysdate.getFullYear();
+
+document.getElementById('currentdate').textContent = currentdate;
+
