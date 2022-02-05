dc34bd3a0cd6fbb66b29291c66bd29b8a78e2c81
ksantos16 <san21087@byui.edu>
2022-02-05 16:31:01 -0500

    "none"

--- lesson5/js/banner.js
+++ lesson5/js/banner.js
@@ -1,10 +1,10 @@
 const bannerDate = new Date()
 const bannerDay = bannerDate.getDay();
-const banner = document.querySelector('.banner');
+const banner = document.querySelector('.banner').style.display = "none";
 
 
 if (bannerDay != 1 || bannerDay != 2) {
-    document.querySelector('.banner').textContent = banner;
+    banner.textContent = banner;
 };
 
 
