df805b92ce4dea07b53a7c4c4ca981c0e2b9b953
ksantos16 <san21087@byui.edu>
2022-02-05 15:20:21 -0500

    js code

--- lesson5/js/banner.js
+++ lesson5/js/banner.js
@@ -1,7 +1,7 @@
 const bannerDate = new Date()
 const bannerDay = bannerDate.getDay();
 
-if (bannerDay != 1 && bannerDay != 2) {
+if (bannerDay === 1 || bannerDay === 2) {
     const banner = document.querySelector('.banner').textContent = banner;
 };
 
