c0fc489d83722065fc8ddda997bfad001859f0ca
ksantos16 <u121364866@byui.edu>
2022-02-13 01:40:18 -0500

    editing pictures

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -1,15 +1,20 @@
-let temperature = parseInt(document.querySelector("#temp").value);
-let windSpeed = parseInt(document.querySelector("#windSpeed").value);
+let temperature = document.querySelector("#temp").innerHTML;
+let windSpeed = document.querySelector("#windSpeed").innerHTML;
+let windChill = document.querySelector("#windChill");
 
 function buildWC(temperature, windSpeed) {
-    let windChill = document.querySelector("#windChill");
 
-
-    let wc = 35.74 + 0.6215 * temperature - 35.75 * Math.pow(windSpeed ** 0.16) + 0.4275 * temperature * Math.pow(windSpeed ** 0.16);
+    let wc = 35.74 + 0.6215 * temperature - 35.75 * (windSpeed ** 0.16) + 0.4275 * temperature * (windSpeed ** 0.16);
 
     wc = Math.floor(wc);
 
-    wc = (wc > temperature) ? temp : wc;
-
-    windChill.innerHTML = wc;
+    return wc
 }
+
+if (temperature <= 50 && windSpeed >= 4.8) {
+    let totalWindChill = buildWC(temperature, windSpeed);
+    windChill.innerHTML = totalWindChill;
+} else {
+    windChill.innerHTML = "N/A";
+}
+
