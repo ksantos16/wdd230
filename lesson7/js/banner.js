7a87ca914a08f8742c699327e2bb269898c4e398
ksantos16 <u121364866@byui.edu>
2022-02-18 20:51:36 -0500

    Changes

---
+++ lesson7/js/banner.js
@@ -0,0 +1,15 @@
+const bannerDate = new Date()
+const bannerDay = bannerDate.getDay();
+
+
+
+if (bannerDay === 1 || bannerDay === 2) {
+    let banner1 = document.querySelector("#banner");
+    banner1.innerHTML;
+} else {
+    let banner2 = document.querySelector("#inactive-banner")
+    banner2.innerHTML;
+}
+
+
+
