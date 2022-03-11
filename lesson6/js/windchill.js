d18a7fc339ba72e169140a153e2b6c9eea50da57
ksantos16 <u121364866@byui.edu>
2022-03-10 20:16:32 -0500

    icons and wether api

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -1,3 +1,27 @@
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?q=Fairbanks,us&units=imperial&APPID=c0b762abe477dc3799ef6dd1a6e3151f";
+fetch(apiURL)
+    .then((response) => response.json())
+    .then((jsObject) => {
+        console.log(jsObject);
+
+
+
+        const currentTemp = document.querySelector('#current-temp')
+
+        currentTemp.textContent = jsObject.main.temp.toFixed(0);
+
+        const iconsrc = `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
+        const desc = jsObject.weather[0].description;
+
+        document.querySelector('#icon-src').textContent = iconsrc;
+        document.querySelector('#weathericon').setAttribute('src', iconsrc);
+        document.querySelector('#weathericon').setAttribute('alt', desc);
+        document.querySelector('figcaption').textContent = desc;
+    });
+
+
+
+
 let temperature = document.querySelector("#temp").innerHTML;
 let windSpeed = document.querySelector("#windSpeed").innerHTML;
 let windChill = document.querySelector("#windChill");
