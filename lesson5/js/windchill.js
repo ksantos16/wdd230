160f07a2eb0c6867f67d4fc27f9d0abbc0d6edd9
ksantos16 <u121364866@byui.edu>
2022-03-14 23:06:59 -0400

    changes 1

--- lesson5/js/windchill.js
+++ lesson5/js/windchill.js
@@ -1,4 +1,5 @@
 const requestURL = 'https://ksantos16.github.io/wdd230/lesson10/json/weather-icons.json';
+
 fetch(requestURL)
     .then(function (response) {
         return response.json();
@@ -16,6 +17,8 @@ const apiURL = "https://api.openweathermap.org/data/2.5/weather?q=Aberdeen,NC,us
 let windChill = document.querySelector("#windChill");
 let temperature = document.querySelector("#temp");
 let windSpeed = document.querySelector("#windSpeed");
+let weathericon = document.querySelector('#icon-src');
+
 
 fetch(apiURL)
     .then((response) => response.json())
@@ -27,11 +30,11 @@ fetch(apiURL)
         windSpeed.textContent = jsObject.wind.speed;
         console.log(windSpeed);
 
-        const iconsrc = jsObject.weather[0].icon;
+        let iconsrc = jsObject.weather[0].icon;
         console.log(iconsrc);
         const desc = jsObject.weather[0].description;
 
-        // document.querySelector('#icon-src').textContent = iconsrc;
+        weathericon = iconsrc;
         document.querySelector('#weathericon').setAttribute('src', iconsrc);
         document.querySelector('#weathericon').setAttribute('alt', desc);
         document.querySelector('figcaption').textContent = desc;
@@ -57,17 +60,8 @@ if (temperature <= 50 && windSpeed >= 4.8) {
 console.log(windChill);
 
 function myWeatherIcon(icon) {
-    if (icon === iconsrc);
+    if (icon === weathericon);
     let myicon = object.myweather.imageurl;
-    document.querySelector('#icon-src').textContent = myicon;
+    weathericon.textContent = myicon;
 }
 
-
-
-
-const ages = [32, 33, 16, 40];
-const result = ages.filter(checkAdult);
-
-function checkAdult(age) {
-    return age >= 18;
-}
