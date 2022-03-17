fc03ab8d2a832ad284871002c93ce95bcd5c1f03
ksantos16 <u121364866@byui.edu>
2022-03-16 22:45:47 -0400

    fixed wether image and fix join membership

--- lesson5/js/windchill.js
+++ lesson5/js/windchill.js
@@ -1,26 +1,18 @@
 
-
-
 const apiURL = "https://api.openweathermap.org/data/2.5/weather?q=Aberdeen,NC,us&units=imperial&APPID=c0b762abe477dc3799ef6dd1a6e3151f";
 let windChill = document.querySelector("#windChill");
 let temperature = document.querySelector("#temp");
 let windSpeed = document.querySelector("#windSpeed");
-let weathericon = document.querySelector('#icon-src');
-let icon1 = ""
-
 
 fetch(apiURL)
     .then((response) => response.json())
     .then((jsObject) => {
-        console.log(jsObject);
 
         temperature.textContent = jsObject.main.temp.toFixed(0);
-        console.log(temperature);
         windSpeed.textContent = jsObject.wind.speed;
-        console.log(windSpeed);
 
-        let iconsrc = jsObject.weather[0].icon;
-        console.log(iconsrc);
+        let iconsrc = `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
+        let weatherimg = jsObject.weather[0].icon;
         const desc = jsObject.weather[0].description;
 
         icon1 = iconsrc;
@@ -28,31 +20,28 @@ fetch(apiURL)
         document.querySelector('#weathericon').setAttribute('alt', desc);
         document.querySelector('figcaption').textContent = desc;
 
-    });
+        const requestURL = 'https://ksantos16.github.io/wdd230/lesson10/json/weather-icons.json';
 
-const requestURL = 'https://ksantos16.github.io/wdd230/lesson10/json/weather-icons.json';
+        fetch(requestURL)
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (object) {
+                // console.log(jsonObject); // temporary checking for valid response and data parsing
+                const icons = object['myweather'];
+                console.log(icons);
 
-fetch(requestURL)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (object) {
-        // console.log(jsonObject); // temporary checking for valid response and data parsing
-        const icons = object['myweather'];
-        console.log(icons);
-
-        function choseIcon(icon) {
-            return icon.icon == icon1;
-        }
-
-        function extractValues(icon) {
-            return icon.imageurl;
-        }
-
-        // Map is used in this example. Look at the Map page to learn about the map function.
-        weathericon = icons.filter((icon) => choseIcon(icon)).map((icon) => extractValues(icon));
+                icons.forEach(image => {
+                    if (image.id === weatherimg) {
+                        let myicon = image.imageurl;
+                        document.querySelector('#weathericon').setAttribute('src', myicon);
+                    }
+                })
+            });
 
     });
+
+
 
 function buildWC(temperature, windSpeed) {
 
@@ -70,15 +59,3 @@ if (temperature <= 50 && windSpeed >= 4.8) {
     windChill.innerHTML = "N/A";
 }
 
-console.log(windChill);
-
-// function myWeatherIcon(icon) {
-//     if (icon === weathericon);
-
-//     let myicon = icon.myweather.imageurl;
-
-//     weathericon.textContent = myicon;
-
-
-// }
-// console.log(myicon);
