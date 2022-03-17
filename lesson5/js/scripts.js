7c1cf328126f169f0a552b3997f38b6812a3c30a
ksantos16 <u121364866@byui.edu>
2022-03-17 18:53:11 -0400

    json

--- lesson5/js/scripts.js
+++ lesson5/js/scripts.js
@@ -110,47 +110,32 @@ function displaySpotlight(spolight) {
     let h4 = document.createElement('h4');
     let p = document.createElement('p');
 
-    let order = ``;
-
-
-    if (prophet.order === 1) {
-        order = `${prophet.order}st`;
-
+    if (spolight.level === "Gold") {
+        // Change the textContent property of the h2 element to contain the prophet's full name
+        h3.textContent = spolight.company;
+        h4.textContent = spolight.slogan;
+        p.textContent = spolight.website;
+    } else {
+        business.remove()
     }
 
-    // Create elements to add to the document
-    let card = document.createElement('section');
-    let h2 = document.createElement('h2');
-    let p1 = document.createElement('p');
-    let p2 = document.createElement('p');
-    let p3 = document.createElement('p');
-    let logo = document.createElement('img');
-
-    card.className = "card"
-    h2.className = "company";
-    p1.className = "para";
-    p2.className = "para";
-    p3.className = "para";
-    logo.className = "directoryLogo";
-
-    // Change the textContent property of the h2 element to contain the prophet's full name
-    h2.textContent = `${business.company}`;
-    p1.textContent = `${business.address}`;
-    p2.textContent = `${business.phone}`;
-    p3.textContent = `${business.website}`;
+    //setting classes names to the element created
+    business.className = "spotlight-box1";
+    business.className = "spotlight-box2";
+    business.className = "spotlight-box3";
+    logo.className = "spotlightlogoimg";
 
     // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
-    logo.setAttribute('src', business.imageurl);
-    logo.setAttribute('alt', `${business.company} logo`);
+    logo.setAttribute('src', spolight.imageurl);
+    logo.setAttribute('alt', spolight.alt);
     logo.setAttribute('loading', 'lazy');
 
     // Add/append the section(card) with the h2 element
-    card.appendChild(logo);
-    card.appendChild(h2);
-    card.appendChild(p1);
-    card.appendChild(p2);
-    card.appendChild(p3);
+    business.appendChild(logo);
+    business.appendChild(h3);
+    business.appendChild(h4);
+    business.appendChild(p);
 
-    // Add/append the existing HTML div with the cards class with the section(card)
-    cards.append(card);
+    // Add/append the existing HTML div with the cards class with the section(business)
+    spotlights.append(business);
 }
