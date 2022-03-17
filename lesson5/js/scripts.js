f1b90132cd5158fee20faba5455273eaefe65a95
ksantos16 <u121364866@byui.edu>
2022-03-17 18:14:59 -0400

    2

--- lesson5/js/scripts.js
+++ lesson5/js/scripts.js
@@ -83,3 +83,74 @@ function lastVisit() {
 }
 lastVisit();
 
+
+
+const requestURL = 'https://ksantos16.github.io/wdd230/lesson9/json/directory.json';
+const spotlights = document.querySelector('.spotlight-box');
+
+fetch(requestURL)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (jsonObject) {
+        // console.log(jsonObject); // temporary checking for valid response and data parsing
+        const businesses = jsonObject['directory'];
+        businesses.forEach(displaySpotlight);
+
+    });
+
+
+
+
+function displaySpotlight(spolight) {
+    // Create elements to add to the document
+    let business = document.createElement('section');
+    let h3 = document.createElement('h3');
+    let logo = document.createElement('img');
+    let h4 = document.createElement('h4');
+    let p = document.createElement('p');
+
+    let order = ``;
+
+
+    if (prophet.order === 1) {
+        order = `${prophet.order}st`;
+
+    }
+
+    // Create elements to add to the document
+    let card = document.createElement('section');
+    let h2 = document.createElement('h2');
+    let p1 = document.createElement('p');
+    let p2 = document.createElement('p');
+    let p3 = document.createElement('p');
+    let logo = document.createElement('img');
+
+    card.className = "card"
+    h2.className = "company";
+    p1.className = "para";
+    p2.className = "para";
+    p3.className = "para";
+    logo.className = "directoryLogo";
+
+    // Change the textContent property of the h2 element to contain the prophet's full name
+    h2.textContent = `${business.company}`;
+    p1.textContent = `${business.address}`;
+    p2.textContent = `${business.phone}`;
+    p3.textContent = `${business.website}`;
+
+    // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
+    logo.setAttribute('src', business.imageurl);
+    logo.setAttribute('alt', `${business.company} logo`);
+    logo.setAttribute('loading', 'lazy');
+
+    // Add/append the section(card) with the h2 element
+    card.appendChild(logo);
+    card.appendChild(h2);
+    card.appendChild(p1);
+    card.appendChild(p2);
+    card.appendChild(p3);
+
+    // Add/append the existing HTML div with the cards class with the section(card)
+    cards.append(card);
+}
