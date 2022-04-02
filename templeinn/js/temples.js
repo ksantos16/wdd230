f0954d5b9facfddf5a404d68b9f5d63adbeb170e
ksantos16 <u121364866@byui.edu>
2022-04-02 14:53:56 -0400

    changes

--- templeinn/js/temples.js
+++ templeinn/js/temples.js
@@ -5,76 +5,157 @@ fetch(templeApi)
     .then((response) => response.json())
     .then((jsObject) => {
         console.log(jsObject);
-
-        let weatherCard = jsObject["daily"];
-        console.log(weatherCard);
-        forecast.innerHTML = "";
-        weatherCard.forEach(weatherForcast);
+        const directory = jsObject['directory'];
+        directory.forEach(displayTemples);
 
     });
 
 
 function displayTemples(temple) {
+    console.log(temple);
 
     // Create elements to add to the document
     let card = document.createElement('section');
-    let image = document.createElement('img')
+
+    // Temple Name
     let templeNames = document.createElement('h3');
-    let headings = document.createElement('h4')
-    let information = document.createElement('p');
-    let phone = document.createElement('p');
-    let address = document.createElement('p');
-    let email = document.createElement('p');
-    let likeBtn = document.createElement('img');
-
-    card.className = "card";
-    image.className = "temple-pic";
-    templeNames.className = "temple-name";
-    information.className = "para";
-    phone.className = "icon";
-    address.className = "icon";
-    email.className = "";
-    likeBtn.className = "likeBtn icon";
-
-    // Change the textContent property of the h2 element to contain the prophet's full name
-    templeNames.textContent = `${temple.name}`;
-
-    headings.textContent = `${temple.scheduletitle}`;
-    information.textContent = `${temple.phase}`;
-    information.textContent = `${temple.ordinanceschedule}`;
-    information.textContent = `${temple.sessionschedule}`;
-
-    // Service Info
-    headings.textContent = `${temple.servicetitle}`;
-    information.textContent = `${temple.services}`;
-
-    // Closure Info
-    headings.textContent = `${temple.closuretitle}`;
-    headings.textContent = `${temple.year}`;
-    information.textContent = `${temple.templeclosureschedule}`;
-
-    // History Info
-    headings.textContent = `${temple.historytitle}`;
-    information.textContent = `${temple.history}`;
-
-    // Contact Info
-    phone.textContent = `${temple.phone}`;
-    address.textContent = `${temple.address}`;
-    email.textContent = `${temple.email}`;
 
     // Temple Image
+    let templeImg = document.createElement('img')
+
+    // Schedule Info
+    let schHeadings = document.createElement('h4');
+    let schInformation1 = document.createElement('p');
+    let schInformation2 = document.createElement('p');
+    let schInformation3 = document.createElement('p');
+
+    // Service Info
+    let srvHeadings = document.createElement('h4');
+    let srvInformation = document.createElement('p');
 
 
-    templeImg.setAttribute('src', temple.image);
+    // Closure Info
+    let clsHeadings = document.createElement('h4');
+    let year = document.createElement('h4')
+    let clsInformation = document.createElement('p');
+
+    // History Info
+    let hisHeadings = document.createElement('h4');
+    let hisInformation = document.createElement('p');
+
+    // Contact Info
+    let addressIcon = document.createElement('img');
+    let address = document.createElement('p');
+
+    let phoneIcon = document.createElement('img');
+    let phone = document.createElement('p');
+
+    let emailIcon = document.createElement('img');
+    let email = document.createElement('p');
+
+    // Like Button
+    let likeBtn = document.createElement('img');
+
+    // card.className = "card";
+    // image.className = "temple-pic";
+    // templeNames.className = "temple-name";
+    // information.className = "para";
+    // phone.className = "icon";
+    // address.className = "icon";
+    // email.className = "";
+    // likeBtn.className = "likeBtn icon";
+
+    // Temple Name
+    templeNames.textContent = `${temple.name}`;
+
+    // Temple Image
+    templeImg.setAttribute('src', `${temple.image}`);
     templeImg.setAttribute('alt', `${temple.alt}`);
     templeImg.setAttribute('loading', 'lazy');
 
-    // Add/append the section(card) with the h2 element
-    card.appendChild(logo);
-    card.appendChild(h2);
-    card.appendChild(p1);
-    card.appendChild(p2);
-    card.appendChild(p3);
+    // Information about the temple and it's services --------------------------------------------------
+
+    // Schedule Info
+    schHeadings.textContent = `${temple.scheduletitle}`;
+    schInformation1.textContent = `${temple.phase}`;
+    schInformation2.textContent = `${temple.ordinanceschedule}`;
+    schInformation3.textContent = `${temple.sessionschedule}`;
+
+    // Service Info
+    srvHeadings.textContent = `${temple.servicetitle}`;
+    srvInformation.textContent = `${temple.services}`;
+
+    // Closure Info
+    clsHeadings.textContent = `${temple.closuretitle}`;
+    year.textContent = `${temple.year}`;
+    clsInformation.textContent = `${temple.templeclosureschedule}`;
+
+    // History Info
+    hisHeadings.textContent = `${temple.historytitle}`;
+    hisInformation.textContent = `${temple.history}`;
+
+    // Contact Info of the temple ------------------------------------------------------------------------
+
+    //Address
+
+    // Address Icon
+    addressIcon.setAttribute('src', temple.pinimg);
+    addressIcon.setAttribute('alt', 'address pin icon');
+    addressIcon.setAttribute('loading', 'lazy');
+
+    // Address
+    address.textContent = `${temple.address}`;
+
+
+    // Phone
+
+    // Phone Icon
+    phoneIcon.setAttribute('src', temple.phimg);
+    phoneIcon.setAttribute('alt', 'phone icon');
+    phoneIcon.setAttribute('loading', 'lazy');
+
+    // Phone number
+    phone.textContent = `${temple.phone}`;
+
+
+    // Email
+
+    // Email Icon
+    emailIcon.setAttribute('src', temple.emimg);
+    emailIcon.setAttribute('alt', 'email icon');
+    emailIcon.setAttribute('loading', 'lazy');
+    // Email
+    email.textContent = `${temple.email}`;
+
+    // Add/append the section(card)
+    card.appendChild(templeNames);
+
+    card.appendChild(templeImg);
+
+    card.appendChild(schHeadings);
+    card.appendChild(schInformation1);
+    card.appendChild(schInformation2);
+    card.appendChild(schInformation3);
+
+    card.appendChild(srvHeadings);
+    card.appendChild(srvInformation);
+
+    card.appendChild(clsHeadings);
+    card.appendChild(year);
+    card.appendChild(clsInformation);
+
+    card.appendChild(hisHeadings);
+    card.appendChild(hisInformation);
+
+    card.appendChild(phoneIcon);
+    card.appendChild(phone);
+    card.appendChild(addressIcon);
+    card.appendChild(address);
+    card.appendChild(emailIcon);
+    card.appendChild(email);
+
+    card.appendChild(likeBtn);
+
 
     // Add/append the existing HTML div with the cards class with the section(card)
     temples.append(card);
