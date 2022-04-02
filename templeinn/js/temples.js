91ff0a44efdce0911306ab0d844d6cfd870b13a4
ksantos16 <u121364866@byui.edu>
2022-04-02 12:18:33 -0400

    jj

--- templeinn/js/temples.js
+++ templeinn/js/temples.js
@@ -22,26 +22,32 @@ function displayTemples(temple) {
     let templeNames = document.createElement('h3');
     let headings = document.createElement('h4')
     let information = document.createElement('p');
-    let p2 = document.createElement('p');
-    let p3 = document.createElement('p');
-    let logo = document.createElement('img');
+    let phone = document.createElement('p');
+    let adress = document.createElement('p');
+    let email = document.createElement('p');
+    let likeBtn = document.createElement('img');
 
-    card.className = "card"
-    h2.className = "company";
-    p1.className = "para";
-    p2.className = "para";
-    p3.className = "para";
-    logo.className = "directoryLogo";
+    card.className = "card";
+    image.className = "temple-pic";
+    templeNames.className = "temple-name";
+    information.className = "para";
+    phone.className = "icon";
+    adress.className = "icon";
+    email.className = "";
+    likeBtn.className = "likeBtn icon";
 
     // Change the textContent property of the h2 element to contain the prophet's full name
-    h2.textContent = `${business.company}`;
-    p1.textContent = `${business.address}`;
-    p2.textContent = `${business.phone}`;
-    p3.textContent = `${business.website}`;
+    templeNames.textContent = `${directory.name}`;
+    headings.textContent = `${directory.schedule title
+} `;
+    information.textContent = `${ directory.phone } `;
+    phone.textContent = `${ directory.website } `;
+    adress.textContent = `${ directory.website } `;
+    email.textContent = `${ directory.website } `;
 
     // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
     logo.setAttribute('src', business.imageurl);
-    logo.setAttribute('alt', `${business.company} logo`);
+    logo.setAttribute('alt', `${ business.company } logo`);
     logo.setAttribute('loading', 'lazy');
 
     // Add/append the section(card) with the h2 element
