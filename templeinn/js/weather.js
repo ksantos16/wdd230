bc2771338d3b8700eea1ba90bb9800e5cf0a4be0
ksantos16 <u121364866@byui.edu>
2022-04-05 22:55:21 -0400

    js

--- templeinn/js/weather.js
+++ templeinn/js/weather.js
@@ -15,67 +15,62 @@ fetch(weatherApi)
 
 
 function weatherForcast(weather) {
-    console.log(weather);
-    weather.map((info, idx) => {
-        if (idx <= 2) {
-            let card = document.createElement('section');
-            let day = document.createElement('p');
-            let mintemp = document.createElement('p');
-            let maxtemp = document.createElement('p');
-            let weathercondition = document.createElement('p');
-            let humidity = document.createElement('p');
+    if (weather.length <= 2) {
+        let card = document.createElement('section');
+        let day = document.createElement('p');
+        let mintemp = document.createElement('p');
+        let maxtemp = document.createElement('p');
+        let weathercondition = document.createElement('p');
+        let humidity = document.createElement('p');
 
-            let desc = info.weather[0].description;
-            let date = new Date(info.dt * 1000); //timestamp * 1000
+        let desc = info.weather[0].description;
+        let date = new Date();
 
-            day.textContent = day;
-            mintemp.textContent = `Low: ${info.temp.min.toFixed(0)}° F`;
-            maxtemp.textContent = `High: ${info.temp.max.toFixed(0)}° F`;
-            weathercondition.textContent = desc;
-            humidity.textContent = `Humidity: ${info.humidity.toFixed(0)}`;
+        day.textContent = date;
+        mintemp.textContent = `Low: ${info.temp.min.toFixed(0)}° F`;
+        maxtemp.textContent = `High: ${info.temp.max.toFixed(0)}° F`;
+        weathercondition.textContent = desc;
+        humidity.textContent = `Humidity: ${info.humidity.toFixed(0)}`;
 
-            let weatherimg = info.weather[0].icon;
+        let weatherimg = info.weather[0].icon;
 
-            let iconsrc = `https://openweathermap.org/img/w/${info.weather[0].icon
-                }.png`;
+        let iconsrc = `https://openweathermap.org/img/w/${info.weather[0].icon
+            }.png`;
 
-            icon1 = iconsrc;
-            document.querySelector('.weather-icon-img').setAttribute('src', iconsrc);
-            document.querySelector('.weather-icon-img').setAttribute('alt', desc);
+        icon1 = iconsrc;
+        document.querySelector('.weather-icon-img').setAttribute('src', iconsrc);
+        document.querySelector('.weather-icon-img').setAttribute('alt', desc);
 
-            const request = 'https://ksantos16.github.io/wdd230/templeinn/json/weather-icons.json';
+        const request = 'https://ksantos16.github.io/wdd230/templeinn/json/weather-icons.json';
 
-            fetch(request)
-                .then(function (response) {
-                    return response.json();
+        fetch(request)
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (object) {
+                // console.log(jsonObject); // temporary checking for valid response and data parsing
+                const icons = object['myweather'];
+                console.log(icons);
+
+                icons.forEach(image => {
+                    if (image.id === weatherimg) {
+                        let myicon = image.imageurl;
+                        document.querySelector('.weather-icon-img').setAttribute('src', myicon);
+                        console.log(myicon);
+                    }
+
                 })
-                .then(function (object) {
-                    // console.log(jsonObject); // temporary checking for valid response and data parsing
-                    const icons = object['myweather'];
-                    console.log(icons);
-
-                    icons.forEach(image => {
-                        if (image.id === weatherimg) {
-                            let myicon = image.imageurl;
-                            document.querySelector('.weather-icon-img').setAttribute('src', myicon);
-                            console.log(myicon);
-                        }
-
-                    })
-                });
+            });
 
 
-            card.appendChild(date);
-            card.appendChild(myicon)
-            card.appendChild(mintemp);
-            card.appendChild(maxtemp);
-            card.appendChild(weathercondition);
-            card.appendChild(humidity)
+        card.appendChild(date);
+        card.appendChild(myicon)
+        card.appendChild(mintemp);
+        card.appendChild(maxtemp);
+        card.appendChild(weathercondition);
+        card.appendChild(humidity)
 
-            forecast.append(card);
+        forecast.append(card);
 
-        }
-    })
-
-        .join(' ');
+    }
 }
